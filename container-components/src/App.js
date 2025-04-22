054d46739ef202b6850c4ff192f504f439eac52c
fabiofernandesx <fabio@fabiofernandes.net>
2025-04-22 16:59:48 +0100

    Showing all containers

--- container-components/src/App.js
+++ container-components/src/App.js
@@ -5,6 +5,8 @@ import { BookInfo } from './components/book-info'
 import { ResourceLoader } from './components/resource-loader'
 import { DataSource } from './components/data-source'
 import { DataSourceRender } from './components/data-source-render'
+import { CurrentUserLoader } from './components/current-user-loader'
+import { UserLoader } from './components/user-loader'
 
 const getDataFromServer = async (url) => {
   return fetch(url).then((response) => response.json())
@@ -18,6 +20,18 @@ const GetMessage = ({ message }) => <h1>{message}</h1>
 function App() {
   return (
     <>
+      <h1>Container Components</h1>
+      <h2>Current User Loader</h2>
+      <CurrentUserLoader>
+        <UserInfo />
+      </CurrentUserLoader>
+      <hr />
+      <h2>User Loader</h2>
+      <UserLoader userId={1}>
+        <UserInfo />
+      </UserLoader>
+      <hr />
+      <h2>Resource Loader</h2>
       <ResourceLoader
         resourceUrl={'http://localhost:9090/users/2'}
         resourceName={'user'}
@@ -32,7 +46,7 @@ function App() {
       </ResourceLoader>
 
       <hr />
-
+      <h2>Data Source</h2>
       <DataSource
         getData={() => getDataFromServer('http://localhost:9090/users/1')}
         resourceName={'user'}
@@ -41,13 +55,14 @@ function App() {
       </DataSource>
 
       <hr />
-
+      <h2>Data Source with Render</h2>
       <DataSourceRender
         getData={() => getDataFromServer('http://localhost:9090/books/1')}
         render={(book) => <BookInfo book={book} />}
       />
 
       <hr />
+      <h2>Data Source with Local Storage</h2>
       <DataSource
         getData={() => getDataFromLocalStorage('anyKey')}
         resourceName={'message'}
