1e1d351c4c6b8b691a3ebb379ff78c1e8cf4d8e7
fabiofernandesx <fabio@fabiofernandes.net>
2025-04-23 12:48:57 +0100

    setting up a prop logger wrapper

--- high-order-components/src/App.jsx
+++ high-order-components/src/App.jsx
@@ -1,7 +1,12 @@
+import { logProps } from './components/log-props'
+import { UserInfo } from './components/user-info'
+
+const UserInfoWrapper = logProps(UserInfo)
 const App = () => {
   return (
     <>
       <h1>High Order Components</h1>
+      <UserInfoWrapper />
     </>
   )
 }
