7d1516ffc15b5da7f174a796bdffd17530df9571
fabiofernandesx <fabio@fabiofernandes.net>
2025-04-23 12:01:27 +0100

    Controlled Modal

--- controlled-uncontrolled/src/App.jsx
+++ controlled-uncontrolled/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import { ControlledForm } from './components/controlled-form'
+import { ControlledModal } from './components/controlled-modal'
 import { UncontrolledForm } from './components/uncontrolled-form'
 
 export default function App() {
+  const [showModal, setShowModal] = useState(false)
   return (
     <>
       <div>
@@ -10,6 +13,16 @@ export default function App() {
         <hr />
         <h2>Controlled Form</h2>
         <ControlledForm />
+        <h2>Controlled Modal</h2>
+        <button onClick={() => setShowModal(!showModal)}>
+          {showModal ? 'Close' : 'Open'} Modal
+        </button>
+        <ControlledModal
+          shouldShow={showModal}
+          close={() => setShowModal(false)}
+        >
+          <h4>Modal Content</h4>
+        </ControlledModal>
       </div>
     </>
   )
